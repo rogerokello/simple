5e4528d6afa4eea0966b7ba1998554061001df58
roger <rogerokello@gmail.com>
2019-03-02 23:47:10 +0300

    Refactor home view to display board names

--- simple/boards/views.py
+++ simple/boards/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 
+from .models import Board
+
 
 def home(request):
-    return HttpResponse('Hello, World!')
+    boards = Board.objects.all()
+
+    board_names = [
+      board.name for board in boards
+    ]
+
+    response_html = '<br>'.join(board_names)
+
+    return HttpResponse(response_html)
