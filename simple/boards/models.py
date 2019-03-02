b5b3988ee2ed5b88e0035cd55127b11a7820571a
roger <rogerokello@gmail.com>
2019-03-02 23:31:58 +0300

    Add str rep of Board; save data to db

--- simple/boards/models.py
+++ simple/boards/models.py
@@ -6,6 +6,9 @@ class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
