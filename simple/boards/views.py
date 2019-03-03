7c092dae737e61dfa853d757fc1394eabbebd79f
roger <rogerokello@gmail.com>
2019-03-03 11:28:00 +0300

    Add NotFound exception handler to make tests pass

--- simple/boards/views.py
+++ simple/boards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from .models import Board
 
@@ -10,5 +11,8 @@ def home(request):
 
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
     return render(request, 'topics.html', {'board': board})
