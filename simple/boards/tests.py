61ca6faf477d97178eb8dbcfb9e762a7fc983365
roger <rogerokello@gmail.com>
2019-03-05 15:41:07 +0300

    - Modify .gitignore to ignore sqlite db - Modify topics template to display link to new topic page/form - Write tests to verify board topics view contains links back to   home page and new topic

--- simple/boards/tests.py
+++ simple/boards/tests.py
@@ -50,6 +50,17 @@ class BoardTopicTests(TestCase):
         homepage_url = reverse('home')
         self.assertContains(response, 'href="{0}"'.format(homepage_url))
 
+    def test_board_topics_view_contains_navigation_links(self):
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
+        homepage_url = reverse('home')
+        new_topic_url = reverse('new_topic', kwargs={'pk': 1})
+
+        response = self.client.get(board_topics_url)
+
+        self.assertContains(response, 'href="{0}"'.format(homepage_url))
+        self.assertContains(response, 'href="{0}"'.format(new_topic_url))
+
+
 
 class NewTopicTests(TestCase):
     def setUp(self):
