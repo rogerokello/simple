178843d84437069abfbee419c14a4e633b6c5d36
roger <rogerokello@gmail.com>
2019-03-05 17:38:12 +0300

    Refactor form to display Help text

--- simple/boards/forms.py
+++ simple/boards/forms.py
@@ -4,7 +4,12 @@ from .models import Topic
 
 
 class NewTopicForm(forms.ModelForm):
-    message = forms.CharField(widget=forms.Textarea(), max_length=4000)
+    message = forms.CharField(
+        widget=forms.Textarea(),
+        max_length=4000,
+        help_text='The max length of the text is 4000'
+    )
+
 
     class Meta:
         model = Topic
