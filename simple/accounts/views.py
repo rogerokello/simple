f814228309883be2a1f815bac0edd770b02a438a
roger <rogerokello@gmail.com>
2019-03-12 00:48:10 +0300

    Add logic for signup endpoint

--- simple/accounts/views.py
+++ simple/accounts/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import login as auth_login
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
+
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
