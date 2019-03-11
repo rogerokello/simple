753fbb0afa88bc5dd60d2cb42a243612c2fecc7a
roger <rogerokello@gmail.com>
2019-03-11 23:57:03 +0300

    Add user creation form to be used in signup view

--- simple/accounts/views.py
+++ simple/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 
 def signup(request):
-    return render(request, 'signup.html')
+    form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
