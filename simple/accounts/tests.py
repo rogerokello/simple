26926c6b087c04ab1b8e4b831005d0c2993be877
roger <rogerokello@gmail.com>
2019-03-12 23:24:50 +0300

    Add tests to verify new user is created and authenticated

--- simple/accounts/tests.py
+++ simple/accounts/tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
 from django.urls import resolve
@@ -24,3 +25,32 @@ class SignUpTests(TestCase):
         form = self.response.context.get('form')
         self.assertIsInstance(form, UserCreationForm)
 
+class SuccessfulSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        data = {
+            'username': 'john',
+            'password1': 'abcdef123456',
+            'password2': 'abcdef123456'
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('home')
+
+    def test_redirection(self):
+        '''
+        A valid form submission should redirect the user to the home page
+        '''
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authentication(self):
+        '''
+        Create a new request to an arbitrary page.
+        The resulting response should now have a `user` to its context,
+        after a successful sign up.
+        '''
+        response = self.client.get(self.home_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
